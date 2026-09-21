5b353a0d9b7589361f4f8d3749900c69f21254ac
rishiraj-dev77 <rishiraj86568@gmail.com>
2026-09-22 00:13:46 +0530

    Add Medium Java Solution for 179. Largest Number from LeetCode

---
+++ LeetCode/Patterns/Greedy/179. Largest Number/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String []a = new String[nums.length];
+        for (int i = 0; i<nums.length; i++){
+            a[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(a,(x,y) ->{
+            return(y+x).compareTo(x+y);
+        } );
+        if(a[0].equals("0")){
+            return "0";
+        }
+        String ans = "";
+        for(int i = 0 ;i<a.length; i++){
+            ans = ans + a[i];
+
+        }
+        return ans;
+    }
+}
